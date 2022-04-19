8da2be2b254396c0e5515921a3cf388310c15ab7
Aleksandr Peretertov <tailler2071@yandex.ru>
2022-04-19 14:08:13 +0500

    feat: styles for elements are implemented

--- src/js/main.js
+++ src/js/main.js
@@ -3,15 +3,14 @@ import "../sass/style.scss";
 
 import './lib/lib';
 
-$('button').on('click', function(){
-    $('div').eq(0).toggleClass('active');
+$('#first').on('click', () => {
+    $('div').eq(1).fadeOut(800);
 });
 
-$('div').click(function(){
-    console.log($(this).index());
+$('[data-count="second"]').on('click', () => {
+    $('div').eq(2).fadeOut(800);
 });
 
-// console.log($('div').eq(2).find('.some'));
-// console.log($('.some').closest('.findme'));
-$('.findme').fadeIn(1800);
-console.log($('.more').eq(0).siblings());
+$('button').eq(2).on('click', () => {
+    $('.w-500').fadeOut(800);
+});
