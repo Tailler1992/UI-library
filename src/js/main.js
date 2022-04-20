3e73d209ef6c8d8cafd954cb552ef7064d864454
Aleksandr Peretertov <tailler2071@yandex.ru>
2022-04-20 13:33:37 +0500

    feat: tabs are implemented

--- src/js/main.js
+++ src/js/main.js
@@ -18,7 +18,8 @@ $('button').eq(2).on('click', () => {
 $('.wrap').html(
     `
     <div class="dropdown">
-        <button class="btn btn-primary dropdown-toggle dropdown-toggle-2" id="dropdownMenuButton2">Dropdown button</button>
+        <button class="btn btn-primary dropdown-toggle dropdown-toggle-2" id="dropdownMenuButton2">
+        Dropdown button</button>
         <div class="dropdown-menu" data-toggle-id="dropdownMenuButton2">
             <a href="#" class="dropdown-item">Action</a>
             <a href="#" class="dropdown-item">Action #2</a>
@@ -28,3 +29,36 @@ $('.wrap').html(
 );
 
 $('.dropdown-toggle-2').dropdawn();
+
+$('#trigger').click(() => $('#trigger').createModal({
+    text: {
+        title: 'Modal title',
+        body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum minus doloremque nesciunt enim rem quam corporis? Dolorem pariatur magnam distinctio perferendis. Ratione dolorem voluptates iusto facilis odit veritatis, suscipit voluptatibus!'
+    },
+    btns: {
+        count: 3,
+        settings: [
+            [
+                'Close',
+                ['btn-danger', 'mr-10'],
+                true
+            ],
+            [
+                'Save changes',
+                ['btn-success'],
+                false,
+                () => {
+                    alert('Данные сохранены');
+                }
+            ],
+            [
+                'Another btn',
+                ['btn-warning', 'ml-10'],
+                false,
+                () => {
+                    alert('Hello World');
+                }
+            ]
+        ]
+    }
+}));
